7cda7f4e444f1d7294298c5d3f4a8e8fe73bee2c
Beniamin Krzanowski <31225487+benkrzanowski@users.noreply.github.com>
2019-03-25 22:17:24 -0500

    Delete caesarCipher.js

--- caesarCipher.js
+++
@@ -1,8 +0,0 @@
-function rot13(message) {
-  var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-  return message.replace(/[a-z]/gi, c => b[a.indexOf(c)]);
-}
-
-rot13("test");
-rot13("Test");
